594724d9bea7a0862ad863233560aa7572d42d57
kenzo-spaulding <kenzo.spaulding@gmail.com>
2021-02-09 17:26:32 -0800

    Initializing Repository

---
+++ services/equipment/src/main/java/edu/rentals/backend/equipment/EquipmentApplication.java
@@ -0,0 +1,13 @@
+package edu.rentals.backend.equipment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquipmentApplication.class, args);
+	}
+
+}
---
+++ services/equipment/src/test/java/edu/rentals/backend/equipment/EquipmentApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.rentals.backend.equipment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
