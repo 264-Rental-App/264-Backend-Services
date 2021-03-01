828f4ac3df327c35990492e9280352c5de2dbcbf
kenzo-spaulding <kenzo.spaulding@gmail.com>
2021-02-28 16:58:35 -0800

    Added Dockerfile and Kubernetes manifests

--- infrastructure/kube.tf
+++ infrastructure/kube.tf
@@ -5,15 +5,15 @@ resource "google_service_account" "default_prod" {
 
 resource "google_container_cluster" "kubes" {
   name     = "prod-kubes"
-  location = "us-central1"
+  location = "us-central1-a"
 
   remove_default_node_pool = true
   initial_node_count       = 1
 }
 
 resource "google_container_node_pool" "primary_preemptible_nodes" {
-  name       = "my-node-pool"
-  location   = "us-central1"
+  name       = "kubes-node-pool"
+  location   = "us-central1-a"
   cluster    = google_container_cluster.kubes.name
   node_count = 1
 
--- services/equipment/src/main/java/edu/rentals/backend/equipment/controllers/PostController.java
+++ services/equipment/src/main/java/edu/rentals/backend/equipment/controllers/PostController.java
@@ -23,5 +23,4 @@ public class PostController {
 
         return new PostEquipmentResponse(equipment.getId());
     }
-
 }
