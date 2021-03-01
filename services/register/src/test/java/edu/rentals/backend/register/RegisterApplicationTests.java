ff5ba2bb70a7e8546bd27fba9bcf422a65615191
angeljiang0927@gmail.com <angeljiang0927@gmail.com>
2021-02-28 23:12:54 -0800

    added new register branch

---
+++ services/register/src/test/java/edu/rentals/backend/register/RegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.rentals.backend.register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
